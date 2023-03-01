f2c9f072375a0b968c321156ee0f136a973e32fe
Lotta Melcher <lotta.melcher@a2ei.org>
2023-03-01 12:19:28 +0100

    fix formatting & remove unnecessary comments

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -4,15 +4,15 @@ import "./TextInput.css"
 export default function TextInput(props) {
     return(
         <div className="text-input-section">
-                <p className="text-input-label"> {props.label} </p>
-                <input 
-                    className="text-input"
-                    type="text" 
-                    value={props.value}
-                    onChange={
-                        (e) => props.onUpdate(props.identifier, e.target.value)
-                    }
-                />
-            </div>
+            <p className="text-input-label"> {props.label} </p>
+            <input 
+                className="text-input"
+                type="text" 
+                value={props.value}
+                onChange={
+                    (e) => props.onUpdate(props.identifier, e.target.value)
+                }
+            />
+        </div>
     )
 }

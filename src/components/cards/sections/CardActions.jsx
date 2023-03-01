d92feb596777f4ad266a979fc9c3a72b9117006a
Lotta Melcher <lotta.melcher@a2ei.org>
2023-03-01 14:25:24 +0100

    CardActions into function

--- src/components/cards/sections/CardActions.jsx
+++ src/components/cards/sections/CardActions.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Toggle from 'react-toggle'
 
-class CardActions extends Component {
-    render() {
-        return (
-            <div className='action-icons'>
-                <p>Required</p>
-                <Toggle
-                    className='action-required'
-                    aria-label='required'
-                    defaultChecked={this.props.required}
-                    onChange={(e) => this.props.onRequiredToggle(e)}
-                />
-                <img 
-                    className='hover-scale'
-                    src="https://img.icons8.com/material-outlined/24/000000/copy.png"
-                    alt="copy"
-                    onClick={() => this.props.onCopy()} />
-                <img
-                    className='hover-scale'
-                    src="https://img.icons8.com/material-rounded/24/000000/trash.png" 
-                    alt="delete"
-                    onClick={() => this.props.onDelete()}/>
-            </div>
-        );
-    }
+export default function CardActions(props) {
+    return(
+        <div className='action-icons'>
+            <p>Required</p>
+            <Toggle 
+                className='action-required'
+                aria-label='required'
+                defaultChecked={props.required}
+                onChange={(e) => props.onRequiredToggle(e)}
+            />
+            <img 
+                className='hover-scale'
+                src="https://img.icons8.com/material-outlined/24/000000/copy.png" 
+                alt="copy icon"
+                onClick={() => props.onCopy()} 
+            />
+            <img
+                className='hover-scale'
+                src="https://img.icons8.com/material-rounded/24/000000/trash.png" 
+                alt="delete icon"
+                onClick={() => props.onDelete()}
+            />
+        </div>
+    )
 }
-
-export default CardActions;

1e287d2d527b2d69fea6b07b0ef9e97c22835dda
Lotta Melcher <lotta.melcher@a2ei.org>
2023-03-07 10:21:04 +0100

    Add Tests for CardInfo

--- src/components/CardInfo.test.js
+++ src/components/CardInfo.test.js
@@ -4,10 +4,23 @@ import { render } from "@testing-library/react";
 import CardInfo from "./CardInfo";
 const makeSut = (props) => {
     return render(
-        <CardInfo />
+        <CardInfo 
+            step_type='1'
+            uid='12345'
+            {...props}
+        />
     )
 }
 
 describe('<CardInfo />', () => {
-    it('', () => {})
+    it('renders step type', () => {
+        const { getByText } = makeSut({ step_type: '3' })
+
+        expect(getByText('3')).toBeInTheDocument()
+    })
+    it('renders uid', () => {
+        const { getByText } = makeSut({ uid: 'IAMID' })
+
+        expect(getByText('IAMID')).toBeInTheDocument()
+    })
 })

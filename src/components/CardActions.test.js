2441fe100d004b941e34150cf5bca8a67e0a963d
Lotta Melcher <lotta.melcher@a2ei.org>
2023-03-07 10:16:44 +0100

    add CardActions Tests

--- src/components/CardActions.test.js
+++ src/components/CardActions.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import CardActions from "./CardActions";
 
@@ -15,11 +15,70 @@ const makeSut = (props) => {
 }
 
 describe('<CardActions />', () => {
-    it('renders required toggle label', () => {
-        const { getByText } = makeSut()
+    describe('Toggle: ', () => {
+        it('renders required toggle label', () => {
+            const { getByText } = makeSut()
+    
+            expect(getByText(/Required/)).toBeInTheDocument()
+        })
+    
+        it('renders required toggle initially off', () => {
+            const { container } = makeSut()
+    
+            expect(container.getElementsByClassName('react-toggle')[0]).toHaveAttribute('class', 'react-toggle action-required')
+    
+        })
+    
+        it('renders required toggle on after click', () => {
+            const { container, getByRole } = makeSut()
+    
+            fireEvent.click(getByRole('checkbox'))
+    
+            expect(container.getElementsByClassName('react-toggle')[0]).toHaveAttribute('class', 'react-toggle react-toggle--checked action-required')
+        })
+    
+        it('calls function on click', () => {
+            const mock = jest.fn()
+    
+            const { getByRole } = makeSut({ onRequiredToggle: mock })
+    
+            fireEvent.click(getByRole('checkbox'))
+    
+            expect(mock).toHaveBeenCalled()
+        })
+    })
+    
+    describe('Copy: ', () => {
+        it('should render', () => {
+            const { getByAltText } = makeSut()
 
-        expect(getByText(/Required/)).toBeInTheDocument()
+            expect(getByAltText('copy')).toBeInTheDocument()
+        })
+
+        it('should call onCopy on click', () => {
+            const mock = jest.fn()
+            const { getByAltText } = makeSut({ onCopy: mock })
+
+            fireEvent.click(getByAltText('copy'))
+
+            expect(mock).toHaveBeenCalled()
+        })
+    })
+    describe('Delete: ', () => {
+        it('should render', () => {
+            const { getByAltText } = makeSut()
+
+            expect(getByAltText('delete')).toBeInTheDocument()
+        })
+
+        it('should call onDelete on click', () => {
+            const mock = jest.fn()
+            const { getByAltText } = makeSut({ onDelete: mock })
+
+            fireEvent.click(getByAltText('delete'))
+
+            expect(mock).toHaveBeenCalled()
+        })
     })
 
-    it('renders ')
 })

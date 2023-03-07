75bf660b8ddcd521d74fb4d318f64105fb119f8c
Lotta Melcher <lotta.melcher@a2ei.org>
2023-03-07 10:34:23 +0100

    Add CardSection Tests

--- src/components/CardSection.test.js
+++ src/components/CardSection.test.js
@@ -1,14 +1,50 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 import CardSection from "./CardSection";
 
 const makeSut = (props) => {
     return render(
-        <CardSection />
+        <CardSection 
+            label='label'
+            enabled={true}
+            onClick={jest.fn()}
+            {...props}
+        />
     )
 }
 
 describe('<CardSection />', () => {
-    it('', () => {})
+    it('is shown if enabled', () => {
+        const { container } = makeSut({ enabled:true })
+
+        expect(container.getElementsByClassName('card-section-container')[0]).toBeInTheDocument()
+    })
+
+    it('is not shown if disabled', () => {
+        const { container } = makeSut({ enabled:false })
+
+        expect(container.getElementsByClassName('hidden')[0]).toBeInTheDocument()
+    })
+
+    it('renders label', () => {
+        const { getByText } = makeSut({ label: 'Hello World'})
+
+        expect(getByText(/Hello World/)).toBeInTheDocument()
+    })
+
+    it('renders children', () => {
+        const { getByText } = makeSut({ children: <p>hello world</p>})
+
+        expect(getByText(/hello world/)).toBeInTheDocument()
+    })
+
+    it('calls function on click', () => {
+        const mock = jest.fn()
+        const { getByRole } = makeSut({ onClick: mock })
+
+        fireEvent.click(getByRole('img'))
+
+        expect(mock).toHaveBeenCalled()
+    })
 })

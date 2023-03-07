9ab881f645aa34de24452b65ef622f01be527db2
Lotta Melcher <lotta.melcher@a2ei.org>
2023-03-07 10:43:11 +0100

    add editor tests

--- src/components/Editor.test.js
+++ src/components/Editor.test.js
@@ -5,10 +5,21 @@ import Editor from "./Editor";
 
 const makeSut = (props) => {
     return render(
-        <Editor />
+        <Editor {...props}/>
     )
 }
 
 describe('<Editor />', () => {
-    it('', () => {})
+    it('creates correct classes', () => {
+        const { container } = makeSut()
+
+        expect(container.getElementsByClassName('editor-layout')).toHaveLength(1)
+        expect(container.getElementsByClassName('editor-flex')).toHaveLength(1)
+    })
+
+    it('allows children to be added', () => {
+        const { getByText } = makeSut({ children:<p>hello world</p> })
+
+        expect(getByText(/hello world/)).toBeInTheDocument()
+    })
 })

c230c520251d82b6e29e869e8a0460a873944168
Lotta Melcher <lotta.melcher@a2ei.org>
2023-03-07 10:47:13 +0100

    Add Footer Tests

--- src/components/Footer.test.js
+++ src/components/Footer.test.js
@@ -5,10 +5,29 @@ import Footer from "./Footer";
 
 const makeSut = (props) => {
     return render(
-        <Footer />
+        <Footer {...props}/>
     )
 }
 
 describe('<Footer />', () => {
-    it('', () => {})
+    it('renders Help link', () => {
+        const { getByText } = makeSut()
+
+        expect(getByText('Help')).toBeInTheDocument()
+        expect(getByText('Help')).toHaveAttribute('href', '/')
+    })
+    it('renders About link', () => {
+        const { getByText } = makeSut()
+
+        expect(getByText('About')).toBeInTheDocument()
+        expect(getByText('About')).toHaveAttribute('href', '/')
+
+    })
+    it('renders A2EI link', () => {
+        const { getByText } = makeSut()
+
+        expect(getByText('A2EI')).toBeInTheDocument()
+        expect(getByText('A2EI')).toHaveAttribute('href', '/')
+
+    })
 })
